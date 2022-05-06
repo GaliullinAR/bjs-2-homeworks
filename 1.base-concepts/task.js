0d5bc26225ed63c0c5a61d95ca0d753b0d7a29e6
Galiullin Azat <galiullin.ar651@gmail.com>
2022-05-06 09:30:01 +0300

    Выполнил первую задачу

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,25 @@
+"use strict"
+
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
+  let discriminant;
   // код для задачи №1 писать здесь
+  discriminant = (Math.pow(b, 2)) - (4 * a * c);
+  console.log(Math.pow(b, 2));
+  console.log(discriminant);
+  if (discriminant < 0) {
+    console.log(arr);
+    return arr;
+    
+  } else if (discriminant === 0) {
+    arr.push(-b / (2 * a));
+    console.log(arr);
+  } else {
+    arr.push((-b + Math.sqrt(discriminant)) / (2 * a), (-b - Math.sqrt(discriminant)) / (2 * a));
+    console.log(arr);
+  }
+  
+  
   return arr; // array
 }
 
@@ -11,3 +30,4 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
 
   return totalAmount;
 }
+
