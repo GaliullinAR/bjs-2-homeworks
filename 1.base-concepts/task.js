8134b5b319384c0d51523148a9a4e83a19a3c053
Galiullin Azat <galiullin.ar651@gmail.com>
2022-05-06 17:16:38 +0300

    Убрал консоль лог с первой задачи

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -5,21 +5,14 @@ function solveEquation(a, b, c) {
   let discriminant;
   // код для задачи №1 писать здесь
   discriminant = (Math.pow(b, 2)) - (4 * a * c);
-  console.log(Math.pow(b, 2));
-  console.log(discriminant);
   if (discriminant < 0) {
-    console.log(arr);
     return arr;
-    
   } else if (discriminant === 0) {
     arr.push(-b / (2 * a));
     console.log(arr);
   } else {
     arr.push((-b + Math.sqrt(discriminant)) / (2 * a), (-b - Math.sqrt(discriminant)) / (2 * a));
-    console.log(arr);
   }
-  
-  
   return arr; // array
 }
 
