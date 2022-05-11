b8878734e2019ff5b5c40c57b79e45b96116441c
Galiullin Azat <galiullin.ar651@gmail.com>
2022-05-11 20:47:15 +0300

    сделал второе задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,7 @@
-"use strict"
+
 
 function solveEquation(a, b, c) {
+  "use strict";
   let arr = [];
   let discriminant;
   // код для задачи №1 писать здесь
@@ -17,10 +18,32 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+  "use strict";
+    // код для задачи №2 писать здесь
+  if (typeof percent === 'string') return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`
+  if (typeof contribution === "string")
+    return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+  if (typeof amount === "string")
+    return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+
   let totalAmount;
+  let today = new Date(2022, 5, 6);
+  let numberOfMonths =
+    (date.getFullYear() - today.getFullYear()) * 12 + (date.getMonth() - today.getMonth()) + 1;
+  
+  let loanAmount = amount - contribution;
+  let percentOfMonth = percent / 12 / 100;
 
-  // код для задачи №2 писать здесь
+  let totalMonphlyPayment =
+    loanAmount *
+    (percentOfMonth + percentOfMonth / (Math.pow(1 + percentOfMonth, numberOfMonths) - 1));
+  totalAmount = totalMonphlyPayment * numberOfMonths;
 
+  totalAmount = totalAmount.toFixed(2);
+  totalAmount = Number(totalAmount);
+
+  console.log(totalAmount);
+  
   return totalAmount;
 }
 
