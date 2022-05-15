e268f64ac5fcb0b86cbcb17f2d20e15949fb3b9e
GaliullinAR <galiullin.ar651@gmail.com>
2022-05-15 22:46:45 +0300

    Выполнид дз по теме функции

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -4,23 +4,55 @@ function getArrayParams(arr) {
 
   // Ваш код
 
+  let result = function getParams(arr) {
+    console.log(arr)
+    min = Infinity;
+    max = -Infinity;
+    sum = 0;
+    
+    for (let i = 0; i < arr.length; i++) {
+      
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+      
+    sum += arr[i];
+      
+    }
+    
+    return { min: min, max: max, sum: sum };
+  }
+
+  result(arr);
+  avg = sum / arr.length;
+  avg = Number(avg.toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
+  let sum = 0;
+  
   // Ваш код
-
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i]
+  }
+  
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
+  let max = -Infinity;
   // Ваш кода
   // for ...
+  for (let i = 0; i < arrOfArr.length; i++) {
+    if (max < func(arrOfArr[i])) {
+      max = func(arrOfArr[i]);
+    }
+  }
   
   return max;
 }
@@ -28,4 +60,20 @@ function makeWork(arrOfArr, func) {
 // Задание 3
 function worker2(arr) {
   // Ваш код
+  let max = -Infinity;
+  let min = Infinity;
+  let sum;
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+    if (min > arr[i]) {
+      min = arr[i];
+    }
+  }
+  sum = max - min;
+
+  
+  return Math.abs(sum);
 }
