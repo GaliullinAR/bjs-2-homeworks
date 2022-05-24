058d168ee267290ba81e40ced89acdef98b9479b
GaliullinAR <galiullin.ar651@gmail.com>
2022-05-24 13:22:52 +0300

    Выполнил задачу с объектами

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,51 @@
 function Student(name, gender, age) {
     // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+
+let student1 = new Student('Azat', 'male', 25);
+let student2 = new Student('Petya', 'male', 22);
+let student3 = new Student('Sasha', 'male', 18);
 
 Student.prototype.setSubject = function (subjectName) {
   //ваш код
+  this.subject = subjectName;
 }
 
 // ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...marks) {
+  if (this.marks === undefined) {
+    this.marks = [...marks];
+  } else {
+    this.marks.push(...marks);
+  }
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+
+  for (let item of this.marks) {
+    console.log(item);
+    sum += item;
+  }
+  return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  this.excluded = reason;
+
+  delete this.subject;
+  delete this.marks;
+}
+
+
